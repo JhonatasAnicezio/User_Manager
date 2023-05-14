ea845cd4b095827a45a1c4edbadba80e3216a528
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-14 20:59:21 -0300

    feat(components/Form): create title for Form components

---
+++ src/components/Form/title.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  title: string
+}
+
+export function Title({ title }: Props) {
+  return (
+    <div>
+        <h1 className="flex justify-center mt-9 text-2xl py-5 font-extrabold">
+          {title}
+        </h1>
+    </div>
+  )
+}
