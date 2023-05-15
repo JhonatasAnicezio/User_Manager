4e78a361b14efec56234c8d0fbde7a5a497f8d53
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-14 21:01:09 -0300

    feat: create "hub" for components at Form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,11 @@
+import { Input } from "./input";
+import { Title } from "./title";
+import { Button } from "./button";
+import { Transiton } from "./transition";
+
+export const Form = {
+  Title,
+  Input,
+  Button,
+  Transiton,
+};
