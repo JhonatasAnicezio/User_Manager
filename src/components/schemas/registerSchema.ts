41986d932de70ba47595fc505ce5ef62f3f8a781
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-14 21:02:21 -0300

    feat: create register Schema

---
+++ src/components/schemas/registerSchema.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+  name: z.string().nonempty({
+    message: 'O nome é obrigatório',
+  }).transform(name => {
+    return name
+      .trim()
+      .split(' ')
+      .map(word => word[0].toLocaleUpperCase().concat(word.substring(1)))
+      .join(' ')
+  }),
+  email: z.string().nonempty({
+    message: 'O e-mail é obrigatório',
+  }).email({
+    message: 'Formato de e-mail inválido',
+  }).toLowerCase(),
+  password: z.string().nonempty({
+    message: 'A senha é obrigatória',
+  }).min(6, {
+    message: 'A senha precisa ter no mínimo 6 caracteres',
+  }),
+});
+
+export type registerData = z.infer<typeof registerSchema>;
