ef2669b3085273e964d8f0fdcec20654ad68beb4
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-15 20:19:11 -0300

    feat: add funcitons for register

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -3,12 +3,15 @@ import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { registerData, registerSchema } from "./schemas/registerSchema";
 import { Form } from '../components/Form';
+import { useContext } from "react";
+import { AuthContext } from "@/context/AuthContext";
 
 type Props = {
   setRegisteredUser: (value: boolean) => void,
 }
 
 export function Register({ setRegisteredUser }: Props) {
+  const { register, invalid, setInvalid } = useContext(AuthContext);
 
   const registerForm = useForm<registerData>({
     resolver: zodResolver(registerSchema),
@@ -16,28 +19,46 @@ export function Register({ setRegisteredUser }: Props) {
 
   const { handleSubmit, formState: { errors } } = registerForm;
 
-  const register = (data: any) => {
-    console.log(JSON.stringify(data, null, 2));
-  }
+  const registerUser = async (data: registerData) => {
+    await register({ ...data, role: 'user' });
+  };
 
   return (
     <FormProvider {...registerForm}>
       <form
-        onSubmit={ handleSubmit(register) }
+        onSubmit={ handleSubmit(registerUser) }
         className="my-auto flex flex-col items-center pb-16 gap-5 w-72 shadow-lg bg-white"
       >
         <Form.Title title="USER MANAGER" />
         <div>
-          <Form.Input name="name" type='name' placeholder='Name'/>
+          <Form.Input
+            name="name"
+            type='name'
+            placeholder='Name'
+            onClick={() => setInvalid({...invalid, validate: false})}
+          />
           {errors.name && <span className="text-red-500 text-xs">{errors.name.message}</span>}
         </div>
         <div>
-          <Form.Input name="email" type='email' placeholder='Email'/>
+          <Form.Input
+            name="email"
+            type='email'
+            placeholder='Email'
+            onClick={() => setInvalid({...invalid, validate: false})}
+          />
           {errors.email && <span className="text-red-500 text-xs">{errors.email.message}</span>}
         </div>
         <div>
-          <Form.Input name="password" type='password' placeholder='Password'/>
+          <Form.Input
+            name="password"
+            type='password'
+            placeholder='Password'
+            onClick={() => setInvalid({...invalid, validate: false})}
+          />
           {errors.password && <span className="text-red-500 text-xs">{errors.password.message}</span>}
+        </div>
+        <div>
+          {invalid.validate && <span className="text-red-500 text-xs">{invalid.message}</span>}
         </div>
         <div>
           <Form.Button text="Register" />
