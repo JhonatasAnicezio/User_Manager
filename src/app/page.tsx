d2f1c26c26ad541fd9cfdd79c6a78cc8f429a218
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-14 21:04:52 -0300

    feat: add components Form for principal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+'use client'
+import { Login } from "@/components/login";
+import { Register } from "@/components/register";
+import { useState } from "react";
+
 export default function Home() {
+  const [registeredUser, setRegisteredUser] = useState(true); 
+
   return (
-    <main>Bem vindo ao projeto User Manager!</main>
+    <main className="flex justify-center h-screen">
+      {registeredUser?
+        <Login setRegisteredUser={setRegisteredUser} /> :
+        <Register setRegisteredUser={ setRegisteredUser } />
+      }
+    </main>
   )
 }
