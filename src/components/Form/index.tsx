cb6b4f227a2bbe32848a89a61c5cfca78e870628
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-19 21:18:23 -0300

    feat: add Label for "form hub"

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,10 +2,12 @@ import { Input } from "./input";
 import { Title } from "./title";
 import { Button } from "./button";
 import { Transiton } from "./transition";
+import { Label } from "./label";
 
 export const Form = {
   Title,
   Input,
   Button,
   Transiton,
+  Label,
 };
