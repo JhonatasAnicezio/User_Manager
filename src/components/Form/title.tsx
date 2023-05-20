75b14ba4c384266c6ee9c68c1737cff8b103deab
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-19 21:17:31 -0300

    feat: alter title

--- src/components/Form/title.tsx
+++ src/components/Form/title.tsx
@@ -4,10 +4,8 @@ type Props = {
 
 export function Title({ title }: Props) {
   return (
-    <div>
-        <h1 className="flex justify-center mt-9 text-2xl py-5 font-extrabold">
-          {title}
-        </h1>
-    </div>
+    <h1 className="flex justify-center mt-9 text-2xl py-5 font-extrabold">
+      {title}
+    </h1>
   )
 }
