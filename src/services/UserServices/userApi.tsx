1a5025bce7f008aba373f0c97e740908ec17bf10
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-22 17:59:00 -0300

    feat: add func getAllUsers

--- src/services/UserServices/userApi.tsx
+++ src/services/UserServices/userApi.tsx
@@ -2,7 +2,7 @@ import { loginData } from '@/components/schemas/loginSchema';
 import { api } from './api';
 import { AxiosResponse } from 'axios';
 import { registerData } from '@/components/schemas/registerSchema';
-import { ResponsePost, User } from '@/interfaces/IAuthContext';
+import { ResponseGetUsers, ResponsePost, User } from '@/interfaces/IAuthContext';
 
 export async function postLogin({ email, password }: loginData) {
   const { data }: AxiosResponse<ResponsePost | string> = await api.post<AxiosResponse>('/login', { email, password })
@@ -28,6 +28,21 @@ export async function postRegister({ name, email, password, role }: registerData
 
 export async function getUser(token: string): Promise<User> {
   const { data }: AxiosResponse<User> = await api.get<AxiosResponse>('/me', {
+    headers: {
+      'Authorization': `${token}`
+    }
+  })
+    .catch((error) => {
+      if(error.response) {
+        return error.response;
+      }
+    })
+
+  return data;
+}
+
+export async function getAllUsers(token: string): Promise<ResponseGetUsers> {
+  const { data }: AxiosResponse<ResponseGetUsers> = await api.get<AxiosResponse>('/', {
     headers: {
       'Authorization': `${token}`
     }
