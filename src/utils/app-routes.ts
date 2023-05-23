49231aea9934ca526df92258e07a86e2baa5492c
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-23 16:55:24 -0300

    feat: create APP_ROUTES

---
+++ src/utils/app-routes.ts
@@ -0,0 +1,17 @@
+export const APP_ROUTES = {
+  private: {
+    admin: {
+      manager: {
+        name: '/admin'
+      },
+    },
+    user: {
+      comum: {
+        name: '/user',
+      }
+    }
+  },
+  public: {
+    home: '/', 
+  }
+};
