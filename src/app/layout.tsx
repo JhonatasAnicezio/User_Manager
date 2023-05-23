7a4621ba65efc21597920dcfbd31d943c4452070
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-23 17:12:13 -0300

    feat: add check PublicPage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
+'use client'
 import { AuthProvider } from '@/context/AuthContext';
 import './globals.css'
 
 import { Inter } from 'next/font/google';
+import { usePathname } from 'next/navigation';
+import { PrivateRoute } from '@/components/PrivateRoute';
+import { checkIsPublicRoute } from '@/utils/checkRoutes';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'User Manager',
-  description: 'Better organize your users',
-}
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const pathName = usePathname();
+
+  const isPublicPage = checkIsPublicRoute(pathName);
+
   return (
     <AuthProvider>
       <html lang="en" className={inter.className}>
         <body className="bg-gradient-to-b from-blue-900 to-blue-400">
-          {children}
+          {isPublicPage && children}
+          {!isPublicPage && <PrivateRoute>{children}</PrivateRoute>}
         </body>
       </html>
     </AuthProvider>
