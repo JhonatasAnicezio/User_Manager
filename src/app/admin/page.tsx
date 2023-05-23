e5a5a4a3be96cefa65b1563729442a76b3bb68b5
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-05-23 11:19:14 -0300

    feat: add register and admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,18 @@
+import { Header } from "@/components/Header";
+import { RegisterAdmin } from '../../components/Form/FormAdmin';
+import { TableAdmin } from "@/components/Table/TableAdmin";
+import { QueryProvider } from "@/context/QueryContext";
+
 export default function Admin() {
   return (
-    <main>Hello Admin!</main>
+    <main className="h-screen">
+      <Header />
+      <div className="flex flex-col p-14 gap-14 justify-center">
+        <QueryProvider>
+          <RegisterAdmin />
+          <TableAdmin />
+        </QueryProvider>
+      </div>
+    </main>
   )
 }
